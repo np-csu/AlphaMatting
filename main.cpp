eb7b899c25605a8a440eca34eed86a5ca1002e27
nipan <nipan1988@gmail.com>
2015-03-09 19:29:33 +0800

    take away time-counting in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,28 @@
  * 2014年1月7日
 */
 #include "sharedmatting.h"
-#include <time.h>
 #include <string>
 
 using namespace std;
 
 int main()
 {
-    double sum = 0;;
     char fileAddr[64] = {0};
 
-    int n;
-    for (n = 1; n < 28; ++n) {
+    for (int n = 1; n < 28; ++n) {
         SharedMatting sm;
-        clock_t start, finish;
-        start = clock();
+
         sprintf(fileAddr, "input/GT%d%d.png", n/10, n%10);
         sm.loadImage(fileAddr);
+
         sprintf(fileAddr, "trimap/trimap1/GT%d%d.png", n/10, n%10);
         sm.loadTrimap(fileAddr);
+
         sm.solveAlpha();
+
         sprintf(fileAddr, "result/GT%d%d.png", n/10, n%10);
         sm.save(fileAddr);
-        finish = clock();
-        cout <<  "Time used by processing GT" << n/10 << n%10 << ".png: "
-             << double(finish - start) / CLOCKS_PER_SEC << endl;
-        sum += double(finish - start) / CLOCKS_PER_SEC;
     }
-    cout << "Time used per image( 27 images): " << sum / 27 << endl;
 
     return 0;
 }
